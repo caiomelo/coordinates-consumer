e1f30f9d3ed6753a84123cca3dae2ec57749049b
Caio Albuquerque Melo <caio.alb.melo@gmail.com>
2018-11-14 01:52:33 -0300

    Added service to receive coordinates and define what to do with them

--- src/main/java/com/greenmile/consumer/service/coordinates/VehicleCoordinatesServiceImpl.java
+++ src/main/java/com/greenmile/consumer/service/coordinates/VehicleCoordinatesServiceImpl.java
@@ -1,10 +1,13 @@
 package com.greenmile.consumer.service.coordinates;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.greenmile.consumer.model.route.Route;
+import com.greenmile.consumer.model.route.RouteStatus;
+import com.greenmile.consumer.repository.coordinates.VehicleCoordinatesRepository;
+import com.greenmile.consumer.service.route.RouteService;
+import com.greenmile.consumer.service.route.RouteUpdateService;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,25 +18,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class VehicleCoordinatesServiceImpl implements VehicleCoordinatesService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(VehicleCoordinatesServiceImpl.class);
+    private VehicleCoordinatesRepository repository;
+
+    private RouteService routeService;
+
+    private RouteUpdateService routeUpdateService;
+
+    @Autowired
+    public void setRouteService(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    @Autowired
+    public void setRouteUpdateService(RouteUpdateService routeUpdateService) {
+        this.routeUpdateService = routeUpdateService;
+    }
+
+    @Autowired
+    public void setRepository(VehicleCoordinatesRepository repository) {
+        this.repository = repository;
+    }
 
     @KafkaListener(topics = "coordinates",
             groupId = "coordinates-consumers",
             containerFactory = "coordinatesListenerContainerFactory")
     @Override
     public void receive(VehicleCoordinates coordinates) {
-        try {
-            LOGGER.info("Received coordinates: {}", new ObjectMapper().writeValueAsString(coordinates));
-            //if it' the first coordinate for the route, send status change message
-            //else, if it was already in a stop, verify whether it' finished
-            //if it' finished, send add executed stop message
-            //also if it's finished, verify whether it took longer than the current longest and send longest message
-            //also if it's finished, verify whether it was the last stop and send status update change
-            //
-            //what to do? save new coordinate, verify what kind of update is it and inform info-provider?
-        } catch (JsonProcessingException ex) {
-            LOGGER.warn(("Could not parse coordinates to string"));
+        Route route = routeService.findByVehicleId(coordinates.getVehicleId());
+        if (route != null && !RouteStatus.FINISHED.equals(route.getStatus())) {
+            repository.add(route, coordinates);
+            Set<VehicleCoordinates> savedCoordinates = repository.getAllUntil(route, coordinates.getInstant().getTime());
+            if (routeUpdateService.update(route, savedCoordinates)) {
+                routeService.save(route);
+            }
         }
     }
-
 }
--- src/test/java/com/greenmile/consumer/service/coordinates/VehicleCoordinatesServiceImplTest.java
+++ src/test/java/com/greenmile/consumer/service/coordinates/VehicleCoordinatesServiceImplTest.java
@@ -1,8 +1,24 @@
 package com.greenmile.consumer.service.coordinates;
 
 import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
+import com.greenmile.consumer.model.route.Route;
+import com.greenmile.consumer.model.route.RouteStatus;
+import com.greenmile.consumer.repository.coordinates.VehicleCoordinatesRepository;
+import com.greenmile.consumer.service.route.RouteService;
+import com.greenmile.consumer.service.route.RouteUpdateService;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import org.mockito.Mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 
 /**
  *
@@ -12,15 +28,103 @@ public class VehicleCoordinatesServiceImplTest {
 
     private VehicleCoordinatesServiceImpl service;
 
+    @Mock
+    private VehicleCoordinatesRepository repositoryMock;
+
+    @Mock
+    private RouteService routeServiceMock;
+
+    @Mock
+    private RouteUpdateService routeUpdateServiceMock;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        
         service = new VehicleCoordinatesServiceImpl();
-    }
-    
-    @Test
-    public void testThatItDoesNotExplode() {
-        VehicleCoordinates coordinates = new VehicleCoordinates();
-        service.receive(coordinates);
+        service.setRepository(repositoryMock);
+        service.setRouteService(routeServiceMock);
+        service.setRouteUpdateService(routeUpdateServiceMock);
     }
 
+    @Test
+    public void testThatItDoesNotDoAnythingIfThereIsNoRouteForTheVehicleFromWhichTheCoordinatesWereReceived() {
+        when(routeServiceMock.findByVehicleId("vehicleId")).thenReturn(null);
+
+        VehicleCoordinates coordinates = new VehicleCoordinates();
+        coordinates.setVehicleId("vehicleId");
+
+        service.receive(coordinates);
+
+        verify(repositoryMock, never()).add(any(Route.class), any(VehicleCoordinates.class));
+        verify(repositoryMock, never()).getAllUntil(any(Route.class), anyLong());
+        verify(routeUpdateServiceMock, never()).update(any(Route.class), any(Set.class));
+        verify(routeServiceMock, never()).save(any(Route.class));
+    }
+
+    @Test
+    public void testThatItDoesNotDoAnythingIfTheFoundRouteRelatedToTheVehicleFromWhichTheCoordinatesWereReceivedIsAlreadyFinished() {
+        Route route = new Route();
+        route.setStatus(RouteStatus.FINISHED);
+
+        when(routeServiceMock.findByVehicleId("vehicleId")).thenReturn(route);
+
+        VehicleCoordinates coordinates = new VehicleCoordinates();
+        coordinates.setVehicleId("vehicleId");
+
+        service.receive(coordinates);
+
+        verify(repositoryMock, never()).add(any(Route.class), any(VehicleCoordinates.class));
+        verify(repositoryMock, never()).getAllUntil(any(Route.class), anyLong());
+        verify(routeUpdateServiceMock, never()).update(any(Route.class), any(Set.class));
+        verify(routeServiceMock, never()).save(any(Route.class));
+    }
+
+    @Test
+    public void testThatItAddsCoordinateAndUpdatesRouteIfRouteIsNotFinished() {
+        Route route = new Route();
+        route.setStatus(RouteStatus.IN_PROGRESS);
+
+        when(routeServiceMock.findByVehicleId("vehicleId")).thenReturn(route);
+
+        VehicleCoordinates coordinates = new VehicleCoordinates();
+        coordinates.setVehicleId("vehicleId");
+        coordinates.setInstant(new Date());
+
+        Set<VehicleCoordinates> allCoordinates = new HashSet<>();
+        when(repositoryMock.getAllUntil(route, coordinates.getInstant().getTime())).thenReturn(allCoordinates);
+
+        when(routeUpdateServiceMock.update(route, allCoordinates)).thenReturn(true);
+
+        service.receive(coordinates);
+
+        verify(repositoryMock, times(1)).add(route, coordinates);
+        verify(repositoryMock, times(1)).getAllUntil(route, coordinates.getInstant().getTime());
+        verify(routeUpdateServiceMock, times(1)).update(route, allCoordinates);
+        verify(routeServiceMock, times(1)).save(route);
+    }
+
+    @Test
+    public void testThatItDoesNotUpdateRouteIfNothingOnItWasChanged() {
+        Route route = new Route();
+        route.setStatus(RouteStatus.IN_PROGRESS);
+
+        when(routeServiceMock.findByVehicleId("vehicleId")).thenReturn(route);
+
+        VehicleCoordinates coordinates = new VehicleCoordinates();
+        coordinates.setVehicleId("vehicleId");
+        coordinates.setInstant(new Date());
+
+        Set<VehicleCoordinates> allCoordinates = new HashSet<>();
+        when(repositoryMock.getAllUntil(route, coordinates.getInstant().getTime())).thenReturn(allCoordinates);
+
+        when(routeUpdateServiceMock.update(route, allCoordinates)).thenReturn(false);
+
+        service.receive(coordinates);
+
+        verify(repositoryMock, times(1)).add(route, coordinates);
+        verify(repositoryMock, times(1)).getAllUntil(route, coordinates.getInstant().getTime());
+        verify(routeUpdateServiceMock, times(1)).update(route, allCoordinates);
+        verify(routeServiceMock, never()).save(any(Route.class));
+    }
 }
