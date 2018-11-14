1901b332358371cf7572e89d70dae93165e72132
Caio Albuquerque Melo <caio.alb.melo@gmail.com>
2018-11-14 01:51:11 -0300

    Added attributes to the Route model in order to make it easier to work on it on new coordinates events

--- src/main/java/com/greenmile/consumer/model/route/Route.java
+++ src/main/java/com/greenmile/consumer/model/route/Route.java
@@ -1,11 +1,10 @@
 package com.greenmile.consumer.model.route;
 
+import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -20,15 +19,22 @@ public class Route implements Serializable {
     @Id
     private String id;
 
-    @NotNull(message = "vehicle ID cannot be null")
     @Indexed
     private String assignedVehicle;
 
-    @NotNull(message = "route plan cannot be null")
     private String routePlan;
 
-    @NotEmpty(message = "there must be at least one planned stop")
     private List<PlannedStop> plannedStops;
+
+    private List<PlannedStop> executedStops;
+
+    private RouteStatus status = RouteStatus.PENDING;
+
+    private PlannedStop longest;
+
+    private VehicleCoordinates lastCoordinates;
+
+    private boolean inAStop = false;
 
     public Route() {
         this.id = UUID.randomUUID().toString();
@@ -67,6 +73,59 @@ public class Route implements Serializable {
 
     public void setPlannedStops(List<PlannedStop> plannedStops) {
         this.plannedStops = plannedStops;
+    }
+
+    public List<PlannedStop> getExecutedStops() {
+        if (executedStops == null) {
+            executedStops = new ArrayList<>();
+        }
+        return executedStops;
+    }
+
+    public void setExecutedStops(List<PlannedStop> executedStops) {
+        this.executedStops = executedStops;
+    }
+
+    public RouteStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RouteStatus status) {
+        this.status = status;
+    }
+
+    public PlannedStop getLongest() {
+        return longest;
+    }
+
+    public void setLongest(PlannedStop longest) {
+        this.longest = longest;
+    }
+
+    public boolean isInAStop() {
+        return inAStop;
+    }
+
+    public void setInAStop(boolean inAStop) {
+        this.inAStop = inAStop;
+    }
+
+    public VehicleCoordinates getLastCoordinates() {
+        return lastCoordinates;
+    }
+
+    public void setLastCoordinates(VehicleCoordinates lastCoordinates) {
+        this.lastCoordinates = lastCoordinates;
+    }
+
+    public boolean updateLongestStop() {
+        boolean updated = false;
+        PlannedStop lastExecutedStop = getExecutedStops().get(getExecutedStops().size() - 1);
+        if (lastExecutedStop.getDuration() > getLongest().getDuration()) {
+            setLongest(lastExecutedStop);
+            updated = true;
+        }
+        return updated;
     }
 
 }
---
+++ src/test/java/com/greenmile/consumer/model/route/RouteTest.java
@@ -0,0 +1,20 @@
+package com.greenmile.consumer.model.route;
+
+import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author caioalbmelo
+ */
+public class RouteTest {
+    
+    
+    
+}
