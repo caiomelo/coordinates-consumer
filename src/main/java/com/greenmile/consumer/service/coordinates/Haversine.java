919f9d643d022e0ca436607ad516a02683df070a
Caio Albuquerque Melo <caio.alb.melo@gmail.com>
2018-11-13 23:48:28 -0300

    Added method to calculate the distance between a planned stop and a coordinate

--- src/main/java/com/greenmile/consumer/service/coordinates/Haversine.java
+++ src/main/java/com/greenmile/consumer/service/coordinates/Haversine.java
@@ -1,6 +1,7 @@
 package com.greenmile.consumer.service.coordinates;
 
 import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
+import com.greenmile.consumer.model.route.PlannedStop;
 
 /**
  *
@@ -15,6 +16,23 @@ public class Haversine {
         double longitudeDelta = Math.toRadians(coordinates1.getLongitude() - coordinates2.getLongitude());
 
         double latitudePointOne = Math.toRadians(coordinates1.getLatitude());
+        double latitudePointTwo = Math.toRadians(coordinates2.getLatitude());
+
+        double insideSquareRoot = Math.pow(Math.sin(latitudeDelta / 2), 2)
+                + Math.pow(Math.sin(longitudeDelta / 2), 2)
+                * Math.cos(latitudePointOne)
+                * Math.cos(latitudePointTwo);
+        
+        double twoTimesArcsin = 2 * Math.asin(Math.sqrt(insideSquareRoot));
+        
+        return EARTH_RADIUS * twoTimesArcsin * 1000;
+    }
+    
+    public static double distanceInMeters(PlannedStop plannedStop, VehicleCoordinates coordinates2) {
+        double latitudeDelta = Math.toRadians(plannedStop.getLatitude() - coordinates2.getLatitude());
+        double longitudeDelta = Math.toRadians(plannedStop.getLongitude() - coordinates2.getLongitude());
+
+        double latitudePointOne = Math.toRadians(plannedStop.getLatitude());
         double latitudePointTwo = Math.toRadians(coordinates2.getLatitude());
 
         double insideSquareRoot = Math.pow(Math.sin(latitudeDelta / 2), 2)
--- src/test/java/com/greenmile/consumer/service/coordinates/HaversineTest.java
+++ src/test/java/com/greenmile/consumer/service/coordinates/HaversineTest.java
@@ -1,6 +1,7 @@
 package com.greenmile.consumer.service.coordinates;
 
 import com.greenmile.consumer.model.coordinates.VehicleCoordinates;
+import com.greenmile.consumer.model.route.PlannedStop;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -11,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class HaversineTest {
 
     @Test
-    public void testThatItCalculatesTheDistanceBetweenTwoPointsCorrectly() {
+    public void testThatItCalculatesTheDistanceBetweenTwoCoordinatesCorrectly() {
         VehicleCoordinates coordinates1 = new VehicleCoordinates();
         coordinates1.setLatitude(-3.740376);
         coordinates1.setLongitude(-38.566944);
@@ -32,4 +33,25 @@ public class HaversineTest {
 
     }
 
+    @Test
+    public void testThatItCalculatesTheDistanceBetweenAPlannedStopAndACoordindateCorrectly() {
+        PlannedStop plannedStop = new PlannedStop();
+        plannedStop.setLatitude(-3.740376);
+        plannedStop.setLongitude(-38.566944);
+
+        VehicleCoordinates coordinates1 = new VehicleCoordinates();
+        coordinates1.setLatitude(-3.762392);
+        coordinates1.setLongitude(-38.483516);
+
+        double distance = Haversine.distanceInMeters(plannedStop, coordinates1);
+        assertEquals(9577, distance, 1);
+
+        VehicleCoordinates coordinates2 = new VehicleCoordinates();
+        coordinates2.setLatitude(-3.762052);
+        coordinates2.setLongitude(-38.481321);
+
+        double distance2 = Haversine.distanceInMeters(plannedStop, coordinates2);
+        assertEquals(246, distance2, 1);
+
+    }
 }
